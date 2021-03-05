c743cb4e2a701a6e41191f0e1f6c3581108e4546
Omid <>
2021-03-05 13:30:34 +0100

    to v3 using npm outdated + updated gatsby config

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,8 +7,9 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import { StaticImage } from "gatsby-plugin-image"
 import { useStaticQuery, graphql } from "gatsby"
-import { Parallax, Background } from "react-parallax"
+import { ParallaxProvider, Parallax } from "react-scroll-parallax"
 
 // import StaticIntro from './Intro/StaticIntro'
 import ScrollTopArrow from "./ScrollTop/ScrollTopArrow"
@@ -20,24 +21,6 @@ import "./layout.css"
 import "./style.css"
 
 const Layout = ({ children }) => {
-  const insideStyles = {
-    background: "white",
-    padding: 20,
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%,-50%)",
-  }
-
-  const image1 =
-    "https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-  const image2 =
-    "https://img00.deviantart.net/2bd0/i/2009/276/c/9/magic_forrest_wallpaper_by_goergen.jpg"
-  const image3 =
-    "https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5297440765001_5280261645001-vs.jpg?pubId=5104226627001&videoId=5280261645001"
-  const image4 =
-    "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/empire-state-building-black-and-white-square-format-john-farnan.jpg"
-
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -52,73 +35,16 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <Hero />
-      <SubHeading className="subheading nature" subHeadingText="Nature" />
-      <Parallax bgImage={image1} strength={500}>
-        <div style={{ height: 500 }}>
-          <div style={insideStyles}>HTML inside the parallax</div>
-        </div>
-      </Parallax>
-      <h2>| | |</h2>
-      <Parallax bgImage={image3} blur={{ min: -1, max: 3 }}>
-        <div style={{ height: 500 }}>
-          <div style={insideStyles}>Dynamic Blur</div>
-        </div>
-      </Parallax>
-      <h2>| | |</h2>
-      <Parallax bgImage={image2} strength={-100}>
-        <div style={{ height: 500 }}>
-          <div style={insideStyles}>Reverse direction</div>
-        </div>
-      </Parallax>
-      <SubHeading className="subheading people" subHeadingText="People" />
-      <Parallax
-        bgImage={image4}
-        strength={200}
-        renderLayer={percentage => (
-          <div>
-            <div
-              style={{
-                position: "absolute",
-                background: `rgba(255, 125, 0, ${percentage * 1})`,
-                left: "50%",
-                top: "50%",
-                borderRadius: "50%",
-                transform: "translate(-50%,-50%)",
-                width: percentage * 500,
-                height: percentage * 500,
-              }}
-            />
-          </div>
-        )}
-      >
-        <div style={{ height: 500 }}>
-          <div style={insideStyles}>renderProp</div>
-        </div>
-      </Parallax>
-      <h2>| | |</h2>
-      <Parallax strength={500}>
-        <Background className="custom-bg">
-          <div
-            style={{
-              height: 2000,
-              width: 2000,
-              backgroundImage: "url('https://i.imgur.com/8CV5WAB.png%27)",
-            }}
-          />
-        </Background>
-        <div>
-          <br />
-          custom background component
-          <br />
-          <br />
-          custom background component
-          <br />
-          <br />
-          custom background component
-          <br />
-          <br />
-        </div>
-      </Parallax>
+      <ParallaxProvider>
+        <SubHeading className="subheading nature" subHeadingText="Nature" />
+        <Parallax className="custom-class" y={[-20, 20]} tagOuter="figure">
+          <StaticImage src="../images/hands.png" />
+          <h1>Fuck this shit</h1>
+        </Parallax>
+        <h2>| | |</h2>
+        <SubHeading className="subheading people" subHeadingText="People" />
+      </ParallaxProvider>
+
       <ScrollTopArrow />
       <footer
         style={{
