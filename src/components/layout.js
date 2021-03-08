9ffc9e78be107db983d399f082879cace364f0c5
Fanny Buhlér <fanny.buhler@hyperisland.se>
2021-03-08 15:12:44 +0100

    migrated from gatsby v2 to v3

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import { Parallax, Background } from "react-parallax"
+
+import HeroNew from '../components/Hero/HeroNew'
+import RicolaVideo from '../images/ricola-video.mp4'
+import VideoTest from '../components/Hero/VideoTest'
 
 import StaticIntro from './Intro/StaticIntro'
 import ScrollTopArrow from "./ScrollTop/ScrollTopArrow"
@@ -51,75 +54,11 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <HeroNew src={RicolaVideo} />
+      
       <Hero />
       <StaticIntro />
       <SubHeading className="subheading nature" subHeadingText="Nature" />
-      <Parallax bgImage={image1} strength={500}>
-        <div style={{ height: 500 }}>
-          <div style={insideStyles}>HTML inside the parallax</div>
-        </div>
-      </Parallax>
-      <h2>| | |</h2>
-      <Parallax bgImage={image3} blur={{ min: -1, max: 3 }}>
-        <div style={{ height: 500 }}>
-          <div style={insideStyles}>Dynamic Blur</div>
-        </div>
-      </Parallax>
-      <h2>| | |</h2>
-      <Parallax bgImage={image2} strength={-100}>
-        <div style={{ height: 500 }}>
-          <div style={insideStyles}>Reverse direction</div>
-        </div>
-      </Parallax>
-      <SubHeading className="subheading people" subHeadingText="People" />
-      <Parallax
-        bgImage={image4}
-        strength={200}
-        renderLayer={percentage => (
-          <div>
-            <div
-              style={{
-                position: "absolute",
-                background: `rgba(255, 125, 0, ${percentage * 1})`,
-                left: "50%",
-                top: "50%",
-                borderRadius: "50%",
-                transform: "translate(-50%,-50%)",
-                width: percentage * 500,
-                height: percentage * 500,
-              }}
-            />
-          </div>
-        )}
-      >
-        <div style={{ height: 500 }}>
-          <div style={insideStyles}>renderProp</div>
-        </div>
-      </Parallax>
-      <h2>| | |</h2>
-      <Parallax strength={500}>
-        <Background className="custom-bg">
-          <div
-            style={{
-              height: 2000,
-              width: 2000,
-              backgroundImage: "url('https://i.imgur.com/8CV5WAB.png%27)",
-            }}
-          />
-        </Background>
-        <div>
-          <br />
-          custom background component
-          <br />
-          <br />
-          custom background component
-          <br />
-          <br />
-          custom background component
-          <br />
-          <br />
-        </div>
-      </Parallax>
       <ScrollTopArrow />
       <footer
         style={{
