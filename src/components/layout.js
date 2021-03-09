8a8d48aaa3505657b9a1c0d24886acdd50bf46a9
Fanny Buhlér <fanny.buhler@hyperisland.se>
2021-03-09 11:15:47 +0100

    Fixed videoplayer

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,14 +9,12 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import HeroNew from '../components/Hero/HeroNew'
 import RicolaVideo from '../images/ricola-video.mp4'
-import VideoTest from '../components/Hero/VideoTest'
 
 import StaticIntro from './Intro/StaticIntro'
 import ScrollTopArrow from "./ScrollTop/ScrollTopArrow"
 import SubHeading from "./SubHeading/SubHeading"
-import Hero from "../components/Hero/Hero"
+import Hero from "./Hero/Hero"
 import Header from "./header"
 
 import "./layout.css"
@@ -32,15 +30,6 @@ const Layout = ({ children }) => {
     transform: "translate(-50%,-50%)",
   }
 
-  const image1 =
-    "https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-  const image2 =
-    "https://img00.deviantart.net/2bd0/i/2009/276/c/9/magic_forrest_wallpaper_by_goergen.jpg"
-  const image3 =
-    "https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5297440765001_5280261645001-vs.jpg?pubId=5104226627001&videoId=5280261645001"
-  const image4 =
-    "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/empire-state-building-black-and-white-square-format-john-farnan.jpg"
-
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -54,9 +43,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <HeroNew src={RicolaVideo} />
-      
-      <Hero />
+      <Hero src={RicolaVideo} />
       <StaticIntro />
       <SubHeading className="subheading nature" subHeadingText="Nature" />
       <ScrollTopArrow />
