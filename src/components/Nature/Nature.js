7cd72699c3d90995420a2dae233e808317fd177b
Omid <>
2021-03-09 11:15:44 +0100

    data.json

--- src/components/Nature/Nature.js
+++ src/components/Nature/Nature.js
@@ -3,21 +3,27 @@ import { StaticImage } from "gatsby-plugin-image"
 import { Parallax } from "react-scroll-parallax"
 
 import SubHeading from "../SubHeading/SubHeading"
+import data from "../../data/data.json"
 
-export default function Nature() {
+const Nature = data.map(data => {
   return (
     <>
       <SubHeading className="subheading nature" subHeadingText="Nature" />
       <div className="wrapper">
-        <Parallax y={[-50, 80]} className="leaves">
-          <StaticImage src="../../images/nature/leaves.png" />
-        </Parallax>
-        <Parallax x={[-50, 80]} className="hands">
-          <StaticImage src="../../images/nature/hands.png" />
-        </Parallax>
+        <div className="left-col">
+          <Parallax y={[-50, 80]} className="leaves">
+            <StaticImage src="../../images/nature/leaves.png" />
+          </Parallax>
 
-        <h2>| | |</h2>
+          <Parallax x={[-50, 80]} className="hands">
+            <StaticImage src="../../images/nature/hands.png" />
+          </Parallax>
+        </div>
+        <h3>{data.heading}</h3>
+        <div className="right-col"></div>
       </div>
     </>
   )
-}
+})
+
+export default Nature
