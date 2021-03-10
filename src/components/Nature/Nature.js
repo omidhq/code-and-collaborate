02949cd12abe8bcc13c2e151ece9d363bb1dfddc
Omid <>
2021-03-10 10:31:14 +0100

    griding component

--- src/components/Nature/Nature.js
+++ src/components/Nature/Nature.js
@@ -3,24 +3,26 @@ import { StaticImage } from "gatsby-plugin-image"
 import { Parallax } from "react-scroll-parallax"
 
 import SubHeading from "../SubHeading/SubHeading"
-import data from "../../data/nature.json"
+import natureData from "./nature_data.json"
 
 const Nature = () => {
+  const content = JSON.parse(JSON.stringify(natureData))
+  console.log(content)
   return (
     <>
       <SubHeading className="subheading nature" subHeadingText="Nature" />
-      <div className="wrapper">
+      <div className="component-wrapper">
         <div className="left-col">
           <Parallax y={[-50, 80]} className="leaves">
             <StaticImage src="../../images/nature/leaves.png" />
-          </Parallax>
-
-          <Parallax x={[-50, 80]} className="hands">
             <StaticImage src="../../images/nature/hands.png" />
           </Parallax>
         </div>
-        <h3>{data.heading}</h3>
-        <div className="right-col"></div>
+
+        <div className="right-col">
+          <h3>{content[0].heading}</h3>
+          <p></p>
+        </div>
       </div>
     </>
   )
