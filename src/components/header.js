0872bc3abb168bda6ecffb4a1bd7d0b8f9bdbcd0
Omid <>
2021-03-11 17:00:19 +0100

    text alignment

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,14 @@
 import { Link } from "gatsby"
-import Logo from '../components/image/logo'
+import Logo from "../components/image/logo"
 import React from "react"
 
 const Header = () => (
   <header>
-    <div
-      style={{
-        margin: `0 auto`,
-        padding: `1.45rem 0`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          <Logo alt="logo" filename="logo.png" />
-        </Link>
-      </h1>
-    </div>
+    <h1 style={{ margin: 0 }}>
+      <Link to="/">
+        <Logo alt="logo" filename="logo.png" />
+      </Link>
+    </h1>
   </header>
 )
 
